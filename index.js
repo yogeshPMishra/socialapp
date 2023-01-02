7e8ecc5a9205b277986d0fd7c34c81d6b2de84c7
yogeshPMishra <mail.yogeshpmishra@gmail.com>
2023-01-02 11:34:52 +0530

    version update -v 1.0.1

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const format = require('date-format')
 const app = express()
+// swagger docs 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('swagger.yaml');
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = 4000 || process.env.PORT
-
-app.listen(PORT,()=>{
-    console.log(`Server is Running ar ${PORT}`)
-})
 
 app.get('/',(req, res)=>{
     res.status(200).send('<h1>Hello World!</h1>')
 })
+
 app.get('/api/v1/instagram',(req,res)=>{
     const instasocial = {
           username :"sangrambehera22",
@@ -20,6 +24,7 @@ app.get('/api/v1/instagram',(req,res)=>{
     }
     res.status(200).send([instasocial])
 })
+
 app.get('/api/v1/facebook',(req,res)=>{
     const instasocial = {
           username :"sangrambehera22",
@@ -29,6 +34,7 @@ app.get('/api/v1/facebook',(req,res)=>{
     }
     res.status(200).send([instasocial])
 })
+
 app.get('/api/v1/linkedin',(req,res)=>{
     const instasocial = {
           username :"Sangram Behera",
@@ -37,4 +43,8 @@ app.get('/api/v1/linkedin',(req,res)=>{
           date:format('hh:mm:ss', new Date())
     }
     res.status(200).send([instasocial])
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is Running ar ${PORT}`)
 })
